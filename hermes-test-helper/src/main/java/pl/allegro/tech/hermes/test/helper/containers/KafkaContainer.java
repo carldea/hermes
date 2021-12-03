4a367255783591d908e7602024e763afa7edd06d
Piotr Rzysko <piotr.rzysko@gmail.com>
2021-12-03 17:14:52 +0100

    kafka client update to 2.7.0

--- hermes-test-helper/src/main/java/pl/allegro/tech/hermes/test/helper/containers/KafkaContainer.java
+++ hermes-test-helper/src/main/java/pl/allegro/tech/hermes/test/helper/containers/KafkaContainer.java
@@ -18,6 +18,7 @@ class KafkaContainer extends GenericContainer<KafkaContainer> {
     private static final String START_STOP_SCRIPT = "/kafka_start_stop_wrapper.sh";
     private static final String STARTER_SCRIPT = "/kafka_start.sh";
     private static final int KAFKA_PORT = 9093;
+    private static final int KAFKA_INTERNAL_CLIENT_PORT = 9094;
     private static final int PORT_NOT_ASSIGNED = -1;
     private static final Duration READINESS_CHECK_TIMEOUT = Duration.ofMinutes(360);
 
@@ -25,6 +26,7 @@ class KafkaContainer extends GenericContainer<KafkaContainer> {
     private final BrokerId brokerId;
     private String externalZookeeperConnect = null;
     private int port = PORT_NOT_ASSIGNED;
+    private int advertisedPort;
     private boolean running = false;
 
     KafkaContainer(DockerImageName dockerImageName, Network network, int brokerNum) {
@@ -35,8 +37,10 @@ class KafkaContainer extends GenericContainer<KafkaContainer> {
         this.networkAlias = "broker-" + brokerNum;
         withNetworkAliases(networkAlias);
         withEnv("KAFKA_BROKER_ID", "" + brokerNum);
-        withEnv("KAFKA_LISTENERS", "PLAINTEXT://0.0.0.0:" + KAFKA_PORT + ",BROKER://0.0.0.0:9092");
-        withEnv("KAFKA_LISTENER_SECURITY_PROTOCOL_MAP", "BROKER:PLAINTEXT,PLAINTEXT:PLAINTEXT");
+        withEnv("KAFKA_LISTENERS",
+                "PLAINTEXT://0.0.0.0:" + KAFKA_PORT + ",BROKER://0.0.0.0:9092,INTERNAL_CLIENT://0.0.0.0:" + KAFKA_INTERNAL_CLIENT_PORT
+        );
+        withEnv("KAFKA_LISTENER_SECURITY_PROTOCOL_MAP", "BROKER:PLAINTEXT,PLAINTEXT:PLAINTEXT,INTERNAL_CLIENT:PLAINTEXT");
         withEnv("KAFKA_INTER_BROKER_LISTENER_NAME", "BROKER");
         withEnv("KAFKA_OFFSETS_TOPIC_REPLICATION_FACTOR", "1");
         withEnv("KAFKA_OFFSETS_TOPIC_NUM_PARTITIONS", "1");
@@ -94,9 +98,18 @@ class KafkaContainer extends GenericContainer<KafkaContainer> {
         return self();
     }
 
-    String getBootstrapServers() {
+    KafkaContainer withAdvertisedPort(int advertisedPort) {
+        this.advertisedPort = advertisedPort;
+        return self();
+    }
+
+    String getAddressForExternalClients() {
         checkState(port != PORT_NOT_ASSIGNED, "You should start Kafka container first");
-        return String.format("%s:%s", getContainerIpAddress(), port);
+        return String.format("%s:%s", getContainerIpAddress(), advertisedPort);
+    }
+
+    String getAddressForInternalClients() {
+        return String.format("%s:%s", networkAlias, KAFKA_INTERNAL_CLIENT_PORT);
     }
 
     @Override
@@ -114,7 +127,8 @@ class KafkaContainer extends GenericContainer<KafkaContainer> {
                 return;
             }
             String startScript = readFileFromClasspath("testcontainers/kafka_start.sh")
-                    .replaceAll("<KAFKA_MAPPED_PORT>", port + "")
+                    .replaceAll("<KAFKA_MAPPED_PORT>", advertisedPort + "")
+                    .replaceAll("<KAFKA_INTERNAL_CLIENT_PORT>", KAFKA_INTERNAL_CLIENT_PORT + "")
                     .replaceAll("<BROKER_HOSTNAME>", networkAlias)
                     .replaceAll("<ZOOKEEPER_CONNECT>", externalZookeeperConnect);
             copyScriptToContainer(startScript, this, STARTER_SCRIPT);
